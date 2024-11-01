0508b434a17f39e0628774ea3b2e1855dee4ba00
Priyanka <pri.ppk@gmail.com>
2024-10-31 23:40:29 -0700

    Implement wave_1: include endpoints: POST, GET_all, GET_one, PUT, DELETE, error handling.

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,23 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from ..db import db
+from typing import Optional
+from datetime import datetime
 
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str]
+    description: Mapped[str]
+    completed_at: Mapped[Optional[datetime]] 
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "completed_at": self.completed_at
+        }
+
+    def is_complete(self):
+        # will return True if completed_at is not None and will return False if its None
+        return self.completed_at is not None
+            
--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -1,1 +1,84 @@
-from flask import Blueprint
+from flask import Blueprint, abort, make_response, Response
+from flask import request
+from app.db import db
+from app.models.task import Task
+
+tasks_bp = Blueprint("tasks_bp", __name__, url_prefix="/tasks")
+
+@tasks_bp.post("")
+def create_a_task():
+    request_body = request.get_json()
+
+    validate_missing_attributes(request_body)
+
+    title = request_body["title"]
+    description = request_body["description"]
+    completed_at = request_body["completed_at"]
+    new_task = Task(title=title, description=description, completed_at=None)
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    return new_task.to_dict(), 201
+
+@tasks_bp.get("")
+def get_all_tasks():
+    query = db.select(Task).order_by(Task.id)
+    tasks = db.session.scalars(query)
+
+    tasks_response = [task.to_dict() for task in tasks]
+    return tasks_response
+
+@tasks_bp.get("/<task_id>")
+def get_one_task(task_id):
+    return validate_task(task_id).to_dict()
+
+@tasks_bp.put("/<task_id>")
+def update_one_task(task_id):
+    task = validate_task(task_id)
+    request_body = request.get_json()
+
+    task.title = request_body["title"]
+    task.description = request_body["description"]
+
+    db.session.commit()
+
+    task_response = {
+        "task": {
+            "id": task.id,
+            "title": task.title,
+            "description": task.description,
+            "is_complete": task.completed_at is not None
+        }
+    }
+
+    return (task_response)
+
+@tasks_bp.delete("/<task_id>")
+def delete_task(task_id):
+    task = validate_task(task_id)
+
+    db.session.delete(task)
+    db.session.commit()
+
+    response = {"details": f"Task {task.id} \"{task.title} 🏞\" successfully deleted"}
+
+    return response
+
+def validate_task(task_id):
+    try:
+        task_id = int(task_id)
+    except:
+        abort(make_response({"message": f"task {task_id} should be an int data type"}, 400))
+
+    query = db.select(Task).where(Task.id == task_id)
+    task = db.session.scalar(query)
+
+    if not task:
+        abort(make_response({"message": f"There is no existing task with an id of {task_id}"}, 404))
+    
+    return task
+
+def validate_missing_attributes(request_body):
+    if "title" not in request_body or "description" not in request_body:
+        abort(make_response({"details": "Invalid data"}, 400))
